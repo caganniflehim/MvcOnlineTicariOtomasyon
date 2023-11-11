b011b3fbe5da3ea94d0a6c9cc42c9922a18ccd54
caganniflehim <omercagand@gmail.com>
2023-11-11 16:16:41 +0300

    Initial commit

---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Cariler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Cariler
+    {
+        [Key]
+        [Column(TypeName = "varchar")]
+        [StringLength(30)]
+        public string CariAd { get; set; }
+
+        [Column(TypeName = "varchar")]
+        [StringLength(30)]
+        public string CariSoyad { get; set; }
+        [Column(TypeName = "varchar")]
+        [StringLength(13)]
+        public string CariSehir { get; set; }
+        [Column(TypeName = "varchar")]
+        [StringLength(50)]
+        public string CariMail { get; set; }
+
+
+        public ICollection<SatisHareket> SatisHarekets { get; set; }
+    }
+}
---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/kategori.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class kategori
+    {
+        [Key]
+        public int KategoriId { get; set; }
+        [Column(TypeName = "varchar")]
+        [StringLength(30)]
+        public string KategoriAd { get; set; }
+
+        public ICollection<Urun> Uruns { get; set; }
+
+    }
+}
